a120dad932cc25ae5c6066465b6e41a62783d3cf
jazzosandhu12 <jazzosandhu12@gmail.com>
2020-06-05 14:37:04 -0400

    added dropdown

--- coding.js
+++ coding.js
@@ -3,6 +3,7 @@
   //get all the employees
   const url = 'http://sandbox.bittsdevelopment.com/code1/fetchemployees.php'
 
+
   //create the function for create elements
   function createNode(element){
     return document.createElement(element);
@@ -16,8 +17,20 @@
   .then(response => response.json())
   .then(function(data){
     let employees = data;
-    console.log(employees);
+    //console.log(employees);
     for(var i = 1; i<= Object.keys(employees).length; i++){
+      //create a dropdown list
+      var select = document.createElement('select');
+      select.name = "employees";
+      select.id= "employees";
+        var option = document.createElement('option');
+        option.value = employees[i].employeefname;
+        option.text = employees[i].employeefname;
+        select.appendChild(option);
+      var label = document.createElement('label');
+      label.innerHTML = "Select the employee Name:";
+      label.htmlfor = "employees";
+      document.getElementById('container').appendChild(label).appendChild(select);
       //fetch the picture
       var picture = fetch('http://sandbox.bittsdevelopment.com/code1/employeepics/'+employees[i].employeeid+'.jpg');
       let profile = document.createElement('div');
@@ -25,7 +38,7 @@
       profile.innerHTML += "<h2>"+ employees[i].employeefname + " "+ employees[1].employeelname+"<br /></h2>";
        profile.innerHTML  += employees[i].employeebio +"<br />";
        var roles = employees[i].roles;
-       console.log(roles);
+       //console.log(roles);
        for (var z=0; z< roles.length; z++){
          profile.innerHTML += "<span style='background-color:"+roles[z].rolecolor+"'>"+roles[z].rolename +"  "+"</span>";
        }
